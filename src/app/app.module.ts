401e50826aaf8b6e094441c6462b2527c20b9043
unknown <chbhanu8@gmail.com>
2018-07-26 14:49:22 +0530

    added new functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
+import { NamesComponent } from './names/names.component';
+import { NameTagComponent } from './name-tag/name-tag.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    NamesComponent,
+    NameTagComponent
   ],
   imports: [
     BrowserModule,
